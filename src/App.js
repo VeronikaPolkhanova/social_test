5c4e04eae53b130043683d7e88e1a2c9db3f09e0
VeronikaPolkhanova <halky.boom@gmail.com>
2022-09-15 19:47:47 +0300

    gh-pages

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import Header from './components/header';
 import ProfilePage from "./pages/Profile";
 import NewsPage from "./pages/News";
+import MainPage from './pages/Main';
 
 function App() {
   return (
     <>
       <Header />
       <Routes>
+        <Route path="/" element={<MainPage />} />
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/news" element={<NewsPage />} />
       </Routes>
---
+++ src/pages/Main.js
@@ -0,0 +1,8 @@
+function MainPage() {
+    return (
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <p style={{ color: '#09b7bd', fontSize: '40px', margin: '15% 0' }}>This is main page, tap on burger to get menu</p>
+        </div>
+    )
+}
+export default MainPage;
