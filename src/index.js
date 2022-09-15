54e27d915a014746724587a935ed1d5475cf1671
VeronikaPolkhanova <halky.boom@gmail.com>
2022-09-15 20:43:13 +0300

    gh-pages fix_2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import store from './store';
 import App from './App';
 
@@ -10,9 +10,9 @@ const root = createRoot(container);
 
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 );
 
